ac546cba6afe81a4e4f0f4136032b7911f18befd
yasminaharb <100855224+yasminaharb@users.noreply.github.com>
2022-08-23 02:22:26 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+/* Home typing animation */
+var typed = new Typed('.typing', {
+    strings: [
+      ' Photographer',
+     '  Web developer',
+     '  Graphic designer',
+  
+  ],
+    typeSpeed: 60,
+    backSpeed:60,
+    loop:true
+  });
